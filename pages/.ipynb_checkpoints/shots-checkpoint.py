c1ac36d7d3808fec6167846abae217c52220d865
Beimnet Girma <beimnetgirma47@gmail.com>
2026-01-13 11:57:24 +0300

    update prompt card path

--- pages/.ipynb_checkpoints/shots-checkpoint.py
+++ pages/.ipynb_checkpoints/shots-checkpoint.py
@@ -139,7 +139,7 @@ with open("Prompt Cards/prompt-card-shot-xG-analysis.md", "r") as file:
      # Read the contents of the file
     prompt_card_text = file.read()
 
-load_css("prompt cards/style/python-code.css")
+load_css("Prompt Cards/style/python-code.css")
 st.expander("Prompt card", expanded=False).markdown(prompt_card_text)
 
 #st.markdown("#### Selected Shot Data")
